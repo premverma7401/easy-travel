092a4eb6ce42a25f509414e97bddd3d34b878248
prem <premverma7401@gmail.com>
2022-01-24 11:51:35 +1300

    easy-travel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import { getPlacesData, getWeatherData } from './api/agent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>
+{
+  const [places, setPlaces] = useState([]);
+  const [weather, setWeather] = useState([]);
+  const [filteredPlaces, setFilterPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  useEffect(() =>
+  {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) =>
+    {
+      setCoordinates({ lat: latitude, lng: longitude })
+    })
+  }, [])
+
+  useEffect(() =>
+  {
+    const filterPlaces = places.filter((place) => place.rating > rating);
+    setFilterPlaces(filterPlaces);
+  }, [rating])
+
+
+  useEffect(() =>
+  {
+    if (bounds.sw && bounds.ne)
+    {
+      setLoading(true);
+      getWeatherData(coordinates.lat, coordinates.lng).then((data) =>
+      {
+        setWeather(data);
+        console.log('weather', data);
+      })
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) =>
+      {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilterPlaces([]);
+        console.log(data);
+        setLoading(false);
+      })
+    }
+  }, [type, bounds])
+
+
+  const onLoad = (autoc) =>
+  {
+    setAutocomplete(autoc)
+  }
+  const onPlaceChanged = () =>
+  {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({ lat, lng });
+  }
+
+  return <>
+    <CssBaseline />
+    <Header
+      setCoordinates={setCoordinates}
+      onLoad={onLoad}
+      onPlaceChanged={onPlaceChanged}
+    />
+    <Grid container spacing={3} style={{ width: '100%' }}>
+      <Grid item xs={12} md={4}>
+        <List
+          places={filteredPlaces.length ? filteredPlaces : places}
+          childClicked={childClicked}
+          loading={loading}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
+
+        />
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Map
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          places={filteredPlaces.length ? filteredPlaces : places}
+          setChildClicked={setChildClicked}
+          weatherData={weather}
+
+        />
+
+      </Grid>
+    </Grid>
+  </>;
 }
-
 export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons";
+import Rating from "@material-ui/lab/Rating";
+import useStyle from "./styles";
+import mapStyles from "./mapStyles";
+
+const Map = ({ setCoordinates, coordinates, setBounds, places, setChildClicked, weatherData }) =>
+{
+  const classes = useStyle();
+  const isDesktop = useMediaQuery("(min-width:600px)");
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) =>
+        {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        }}
+        onChildClick={(child) => { setChildClicked(child) }}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={place.latitude}
+            lng={place.longitude}
+            key={i}
+          >
+            {
+              !isDesktop ? (
+                <LocationOnOutlined color="primary" fontSize="large" />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom >
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    alt={place.name} />
+                  <Rating size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )
+            }
+          </div>
+        ))
+        }
+        {
+          weatherData?.list?.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+              <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
+            </div>
+          ))
+        }
+      </GoogleMapReact >
+    </div >
+  );
+};
+
+export default Map;
